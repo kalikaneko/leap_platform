9f8de7b3f323a0504bff3d73304bfe9a66e94d32
varac <varacanero@zeromail.org>
2015-11-05 21:13:31 +0100

    [bug] [jessie] Load needed modules for apache 2.4
    - Related: #6920

--- puppet/modules/site_apache/manifests/module/alias.pp
+++ puppet/modules/site_apache/manifests/module/alias.pp
@@ -1,5 +1,14 @@
+# install mod_alias
 class site_apache::module::alias ( $ensure = present )
 {
 
   apache::module { 'alias': ensure => $ensure }
+
+  # from 2.4, /etc/apache2/mods-enabled/alias.conf contains the "Require"
+  # directive which needs "authz_core" mod enabled
+
+  if ( versioncmp($::apache_version, '2.4') >= 0 ) {
+    apache::module { 'authz_core': ensure => $ensure }
+  }
+
 }
